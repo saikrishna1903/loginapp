f0c559149593d89c96694e311ef390623b27f3db
sathouti <sathouti@LIN66013075.corp.capgemini.com>
2022-04-26 09:59:14 +0530

    first

---
+++ web/src/main/java/com/example/web/WebApplication.java
@@ -0,0 +1,15 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.example.web")
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
